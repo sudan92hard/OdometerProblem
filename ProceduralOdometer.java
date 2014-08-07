f9d4b3cc4aeef4b8cedafbc79f1e39b7d3d0bf4c
vishnu-priya <vishnupriya.c@icicletech.com>
2014-08-07 16:12:07 +0530

    Update ProceduralOdometer.java

--- ProceduralOdometer.java
+++ ProceduralOdometer.java
@@ -9,25 +9,25 @@ public class ProceduralOdometer {
 		}
 		return true;
 	}
-	
+
 	public static String getMinimumDistance(int digits){
 		if(digits <= 0 || digits > 10)
 			return "";
-		
+
 		String number = "";
-		
+
 		for(int i = 0; i < digits; i++){
 			number += i;
 		}		
 		return number;
 	}
-	
+
 	public static String getMaximumDistance(int digits){
 		if(digits <= 0 || digits > 10)
 			return "";
-		
+
 		String number = "";
-		
+
 		for(int i = digits; i > 0; i--){
 			number += (10 - i);
 		}		
@@ -38,21 +38,21 @@ public class ProceduralOdometer {
 	{
 		if(!isValidDistance(distance))
 			return "Invalid Distance";
-		
-		
+
+
 		if(distance.equals(getMaximumDistance(distance.length())))
-			return "";
-			
+			return getMinimumDistance(distance.length());
+
 		int numOfDigits = distance.length();
 		char[] charArrayDistance = distance.toCharArray();
-		
+
 		if(charArrayDistance[numOfDigits - 1] - '0' < 9)
 		{
 			charArrayDistance[numOfDigits - 1] += 1;
 			//System.out.println(new String(charArrayDistance));
 			return new String(charArrayDistance);
 		}
-		
+
 		for(int i = numOfDigits - 1 ; i > 0 ; i--)
 		{
 			if(charArrayDistance[i] - charArrayDistance[i - 1] > 1)
@@ -64,44 +64,26 @@ public class ProceduralOdometer {
 				}
 				break;	
 			}
-			
+
 		}
 		return new String(charArrayDistance);
 	}
-	
-	static List<String> getListOfReadings(String startReading, String endReading)
-        {
-             List<String> readings = new ArrayList<String>();
-             if(!isValidDistance(startReading) || !isValidDistance(endReading))
-             {
-                 return readings;
-             }
-             while(!startReading.equals(endReading)) 
-             {
-                 readings.add(startReading);
-                 startReading = getNextReading(startReading);
-             }
-             readings.add(endReading);
-            return readings;
-        }
-        
-        static String getNthDistance(String startReading, int N)
-        {
-             String reading = startReading;            
-             while(N != 0)
-             {
-                 reading = getNextReading(reading);
-                 N--;
-             }
-            return reading;
-        }
-	
+
+	public static String getNthDistance(String startReading, int N)
+	{
+		while(N != 0)
+		{
+			startReading = getNextReading(startReading);
+			N--;
+		}
+		return startReading;
+	}
+
 	public static void main(String args[]){
 		int digits = 4;
 		String minimumDistance = getMinimumDistance(digits);
 		String maximumDistance = getMaximumDistance(digits);
 		String nextDistance = getNextReading(minimumDistance);
-		List< String > readingList = getListOfReadings(minimumDistance,maximumDistance);
-		System.out.println(readingList);
+		System.out.println(getNthDistance("6789",4));
 	}	
 }
